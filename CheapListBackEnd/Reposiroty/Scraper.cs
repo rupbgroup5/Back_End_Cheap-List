2cdc18118a198d7cd5670eda02c689fd9a776989
OrelKarmi <57489571+OrelKarmi@users.noreply.github.com>
2020-07-21 11:31:27 +0300

    .

--- CheapListBackEnd/Reposiroty/Scraper.cs
+++ CheapListBackEnd/Reposiroty/Scraper.cs
@@ -3,7 +3,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Web;
 
 namespace CheapListBackEnd.Reposiroty
@@ -13,34 +16,43 @@ namespace CheapListBackEnd.Reposiroty
         public static List<string> GetSrcIMG(List<string> arrName)
         {
             List<string> returnValueArr = new List<string>();
-            
-                var driver = new ChromeDriver();
-                driver.Url = "https://www.google.com/imghp";
 
-            foreach (var item in arrName)
+            try
             {
-                var search = driver.FindElementByName("q");
-                search.SendKeys(item);
-                search.SendKeys(Keys.Enter);
-                
-                try
+
+                var driver = new ChromeDriver("../driver/");
+    
+                driver.Url = "https://www.google.com/imghp";
+                foreach (var item in arrName)
                 {
-                    IWebElement img = driver.FindElementByXPath("//*[@id=\"islrg\"]/div[1]/div[1]/a[1]/div[1]/img");
-                    string src = img.GetAttribute("src");
-                    returnValueArr.Add(src);
-                    driver.FindElementByName("q").Clear();
+                    var search = driver.FindElementByName("q");
+                    search.SendKeys(item);
+                    search.SendKeys(Keys.Enter);
+
+                    try
+                    {
+                        IWebElement img = driver.FindElementByXPath("//*[@id=\"islrg\"]/div[1]/div[1]/a[1]/div[1]/img");
+                        string src = img.GetAttribute("src");
+                        returnValueArr.Add(src);
+                        driver.FindElementByName("q").Clear();
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        returnValueArr.Add("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXh6vH////5+vrl7fPv8/f0+fzt9Pbf6vD+/f/j6/Lf6vL///3f6fLr8fbv8/by9vm0HxD7AAACbUlEQVR4nO3c65abIBRAYVHTDALx/d+2VBuDII6zLHro2t/fmEn2HETn2jQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADkG29irNHa4I9G9+qu83B2BTauu01JIIYUUUkghhbcX/ipFTGH3LKOTUmi6Qq/QGQrLovA8Ckuj8LzqC/98A2ZX5YXd2Lajf6beOaTmwm5+tnnsBNZc+NTt/OaNanWT/U5hzYWjWjzyh1VSqK1ONpTPW/fyz66kcHCqixJtHwSqPvvUKgq1D/Rjik61MZzhWPd5aJ3fTfwR6yn+P4XzBFU8RevCwvzPJCoo9IFmepdmPUUdFuqaZzgt0fcUw8T+88Ar/wrSC/UywemgcKE+7Tg/ZNRY8V2bDScYT9G6r+mdu+fOK8guXE8wmaK/Vet6980PPmUXDvEEVXLRCL942lwGkguny0RSmF76//ILuh/SE1JyYXIObk/xzfn17NJH5BZ+LvSHpmjno10yRbmFySYTDHFjivONnZ9iHC+3MLNE31NcJy7zNsn9m9DC9DIRTXG9UD977jTF1UIVWpjbZDJTDM/YeLsRWfjdBKMprj8d8RRFFm5d6Dca31OM99z1diOwMHehT01TTBe0WV00BBYem+AyRbdxcDhFgYX6YN80RZu5LTCSZ9gcLzSqz+xIZvloAgt/MsN8u+QZUkghhRRSSCGFFFJIIYWXFKqv85Towr3fcDpKSy78xygsjcLzKCyNwvPEFCpthxKsVlIKXVeGE1NYHoUUUkghhRRSeFfh2D6u0o53BOpmuOz/RA17f8MHAAAAAAAAAAAAAAAAAAAAAAAAAAAAALjcb3yLQG5tF3tgAAAAAElFTkSuQmCC");
+                        continue;
+                    }
                 }
-                catch (NoSuchElementException)
-                {
-                    returnValueArr.Add("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXh6vH////5+vrl7fPv8/f0+fzt9Pbf6vD+/f/j6/Lf6vL///3f6fLr8fbv8/by9vm0HxD7AAACbUlEQVR4nO3c65abIBRAYVHTDALx/d+2VBuDII6zLHro2t/fmEn2HETn2jQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADkG29irNHa4I9G9+qu83B2BTauu01JIIYUUUkghhbcX/ipFTGH3LKOTUmi6Qq/QGQrLovA8Ckuj8LzqC/98A2ZX5YXd2Lajf6beOaTmwm5+tnnsBNZc+NTt/OaNanWT/U5hzYWjWjzyh1VSqK1ONpTPW/fyz66kcHCqixJtHwSqPvvUKgq1D/Rjik61MZzhWPd5aJ3fTfwR6yn+P4XzBFU8RevCwvzPJCoo9IFmepdmPUUdFuqaZzgt0fcUw8T+88Ar/wrSC/UywemgcKE+7Tg/ZNRY8V2bDScYT9G6r+mdu+fOK8guXE8wmaK/Vet6980PPmUXDvEEVXLRCL942lwGkguny0RSmF76//ILuh/SE1JyYXIObk/xzfn17NJH5BZ+LvSHpmjno10yRbmFySYTDHFjivONnZ9iHC+3MLNE31NcJy7zNsn9m9DC9DIRTXG9UD977jTF1UIVWpjbZDJTDM/YeLsRWfjdBKMprj8d8RRFFm5d6Dca31OM99z1diOwMHehT01TTBe0WV00BBYem+AyRbdxcDhFgYX6YN80RZu5LTCSZ9gcLzSqz+xIZvloAgt/MsN8u+QZUkghhRRSSCGFFFJIIYWXFKqv85Towr3fcDpKSy78xygsjcLzKCyNwvPEFCpthxKsVlIKXVeGE1NYHoUUUkghhRRSeFfh2D6u0o53BOpmuOz/RA17f8MHAAAAAAAAAAAAAAAAAAAAAAAAAAAAALjcb3yLQG5tF3tgAAAAAElFTkSuQmCC");
-                    continue;
-                }
-            }
-           
+
                 driver.Quit();
                 return returnValueArr;
-            
-           
+
+            }
+            catch (Exception e)
+            {
+                returnValueArr.Add(e.Message);
+                return returnValueArr;
+            }
+
 
 
         }
