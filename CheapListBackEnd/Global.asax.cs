034dc9dfcd3391a209df7ae937ed930b834bc36e
OrelKarmi <57489571+OrelKarmi@users.noreply.github.com>
2020-07-23 15:12:47 +0300

    .

--- CheapListBackEnd/Global.asax.cs
+++ CheapListBackEnd/Global.asax.cs
@@ -19,6 +19,7 @@ namespace CheapListBackEnd
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
